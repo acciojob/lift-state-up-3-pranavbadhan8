8d372f8d83d5e88778bb4b1774d75b4b156a1a7a
pranavbadhan8 <pranavbadhan8@gmail.com>
2023-07-21 19:21:13 +0530

    submitting code-Fri Jul 21 2023 19:21:10 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+    let [ans,setAns] = useState("Selected Option");
+
+    let clicking1 = () =>{
+    setAns("Selected Option : Option 1"); 
+    }
+
+    let clicking2 = () =>{
+    setAns("Selected Option : Option 2"); 
+    }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <div className="parent">
+          <h1>Parent Component</h1>
+          <div className="child1">
+              <h2>Child Component 1</h2>
+              <button onClick={clicking1}>Option 1</button>
+           </div>
+           <div className="child2">
+              <h2>Child Component 2</h2>
+              <button onClick={clicking2}>Option 2</button>
+           </div>
+           <p>{ans}</p>
+        </div>
     </div>
   )
 }
